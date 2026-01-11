8a022c51d69d0b18c126b7b6b4c4c92c429df020
shraddhahiremath114 <shraddhahiremath114@gmail.com>
2026-01-11 12:11:41 +0530

    SpringBootApplicationAnnotation

---
+++ annotationsdemo/src/main/java/com/example/annotationsdemo/AnnotationsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.annotationsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationsdemoApplication.class, args);
+	}
+
+}
